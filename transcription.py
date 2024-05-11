39afddef430f4c4b95fa589696e18f69dff15d91
Pxluthor <34409005+pxluthor@users.noreply.github.com>
2024-05-11 00:11:43 -0300

    Update transcription.py

--- transcription.py
+++ transcription.py
@@ -7,9 +7,9 @@ import toml
 
 
 # Carregar configurações do arquivo config.toml
-config = toml.load("config.toml")
+#config = toml.load("config.toml")
 
-st.set_page_config(config_file = config.toml)
+st.set_page_config(config_file = 'config.toml')
 
 # Obter o valor de maxUploadSize do arquivo de configuração
 
